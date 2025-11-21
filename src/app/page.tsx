f51b8510dc78971686bbb7060b50721c59a16d30
Dan Elvey <dan.elvey@trustedhousesitters.com>
2025-11-21 11:39:16 +0000

    commit dis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,6 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the super hot file.
           </h1>
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            A small change
-          </h1>
         </div>
       
       </main>
